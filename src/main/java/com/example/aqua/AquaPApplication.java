ba97249bcf98450317ccab42c148e6c0404d2286
kwon2501 <kwonkyulin25@gmail.com>
2024-12-17 11:36:49 +0900

    Initial commit

---
+++ src/main/java/com/example/aqua/AquaPApplication.java
@@ -0,0 +1,13 @@
+/*package com.example.aqua;*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquaPApplication.class, args);
+	}
+
+}
